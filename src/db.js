71b8142ec29a64bbb365c785b4a82a031030ffbe
UseTravel <pe.phsants0@gmail.com>
2025-05-13 14:22:19 -0300

    alterando .env

--- src/db.js
+++ src/db.js
@@ -2,11 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5435,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  // Importante para Railway (SSL)
 });
 
 module.exports = pool;
+
